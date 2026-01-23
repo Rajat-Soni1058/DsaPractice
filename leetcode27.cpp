0ae9578e1c4a142a04d4e95715f91befa7cd5f80
Rajatsoni896 <rajatsoni896@gmail.com>
2026-01-23 22:28:38 +0530

    leetcode27

---
+++ leetcode27.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+           if(nums[i]!=val) count++;
+           else nums[i]=INT_MAX; 
+        }
+        sort(nums.begin(),nums.end());
+        return count;
+    }
+};
