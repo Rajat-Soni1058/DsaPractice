3b86287f88f9d622560b36848d40ea58d44762a3
Rajatsoni896 <rajatsoni896@gmail.com>
2026-02-10 19:33:42 +0530

    leetcode3719

---
+++ leetcode3719.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            unordered_set<int> o,e;
+            for(int j=i;j<n;j++){
+                
+                if(nums[j]%2!=0) {
+                    o.insert(nums[j]);
+                }
+                else {
+                    e.insert(nums[j]);
+                }
+                if(e.size()==o.size()){
+                    maxi=max(maxi,j-i+1);
+                }
+            }
+        }
+        return maxi;
+
+        
+    }
+};
