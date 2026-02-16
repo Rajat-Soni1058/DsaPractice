0b2bb58e8ca2e43d64ed4ebd0be7b93fbf12f28c
Rajatsoni896 <rajatsoni896@gmail.com>
2026-02-16 14:24:34 +0530

    leetcode190 solution

---
+++ leetcode190.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int reverseBits(int n) {
+        int result=0;
+        if(n==0) return 0;
+        for(int i=1;i<=32;i++){
+           result= result<<1;
+            result=result|(n&1);
+           n= n>>1;
+        }
+        return result;
+        
+    }
+};
