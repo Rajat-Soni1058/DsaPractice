f289b13c11e8aec22bc20ef0e7429e8a4aa3c637
Rajatsoni896 <rajatsoni896@gmail.com>
2026-02-26 22:55:35 +0530

    soltuion leetcode1404

---
+++ leetcode1404.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numSteps(string s) {
+        
+        int count=0;
+        while(s.size()>1){
+            int n=s.size();
+            if(s[n-1]=='0'){
+                s.pop_back();
+            }
+            else {
+                int i=n-1;
+                while(i>=0&&s[i]!='0'){
+                    s[i]='0';
+                    i--;
+                }
+                if(i<0){
+                    s="1"+s;
+                }
+                else{
+                    s[i]='1';
+                }
+
+            }
+            count++;
+
+
+        }
+        return count;
+    }
+};
