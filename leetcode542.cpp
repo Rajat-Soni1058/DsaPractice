223f7ecdda9fd8dc1c990788fc820b2cdc272ddb
Rajatsoni896 <rajatsoni896@gmail.com>
2026-03-14 22:04:24 +0530

    leetcode soltuion 542

---
+++ leetcode542.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        queue<pair<int, int>> q;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) q.push({r, c});
+                else mat[r][c] = -1; // Marked as not processed yet!
+
+        while (!q.empty()) {
+            auto p = q.front(); q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = p.first + DIR[i], nc = p.second + DIR[i+1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[p.first][p.second] + 1;
+                q.push({nr,nc});
+            }
+        }
+        return mat;
+    }
+};
