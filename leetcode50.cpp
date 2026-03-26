7d5b2937fccad2867bf10cf10664c59ae9272146
Rajatsoni896 <rajatsoni896@gmail.com>
2026-03-26 21:15:37 +0530

    leetcode soltuion 50

---
+++ leetcode50.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long k=n;
+if(n<0){
+    x=1/x;
+    k=-1*k;
+}
+   while(k>0){
+    if(k%2==1){
+        ans=ans*x;
+    }
+    x=x*x;
+    k=k/2;
+   }
+   return ans;
+    }
+};
